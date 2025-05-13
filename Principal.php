ac1a5bd0d097f15ea783612570433a63fe531ab0
Javier Nina Paco <keima13234@hotmail.com>
2025-05-13 13:40:53 -0400

    arreglo de importe de clase de responsabilidad

--- Principal.php
+++ Principal.php
@@ -1,15 +1,17 @@
 <?php
 require_once 'ConstructorComida.php';
 require_once 'MetodoEntrega.php';
+require_once 'CadenaResponsabilidad.php';  
 
 
-// Builder
 $comida = (new ConstructorComida())
     ->establecerPlatoPrincipal("Pizza")
     ->establecerAcompañamiento("Papas fritas")
     ->establecerBebida("Coca-Cola")
     ->construir();
 $comida->describir();
+
+
 
 $pedido1 = new Pedido(new ComerEnSitio());
 $pedido1->entregarPedido("Pizza Margarita");
@@ -20,11 +22,14 @@ $pedido2->entregarPedido("Hamburguesa");
 $pedido3 = new Pedido(new EntregaDomicilio());
 $pedido3->entregarPedido("Sushi");
 
+
 $chef = new Chef();
-  $mesero = new Mesero();
-  $cajero = new Cajero();
-  $chef->establecerSiguiente($mesero)->establecerSiguiente($cajero);
-  
-  $chef->manejar("preparar");
-  $chef->manejar("servir");
-  $chef->manejar("cobrar");
+$mesero = new Mesero();
+$cajero = new Cajero();
+$chef->establecerSiguiente($mesero)->establecerSiguiente($cajero);
+
+$chef->manejar("preparar"); 
+$chef->manejar("servir");    
+$chef->manejar("cobrar");  
+?>
+z
